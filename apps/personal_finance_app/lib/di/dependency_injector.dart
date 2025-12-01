68e53f1aa6d49bb918ea53c5c974c15d222f79f4
Adrian Ruiz <addsdeveloper@gmail.com>
2025-12-01 09:20:13 -0500

    feat(personal_finance_app): configure firebase app check and inject transactions dependencies

--- apps/personal_finance_app/lib/di/dependency_injector.dart
+++ apps/personal_finance_app/lib/di/dependency_injector.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:get_it/get_it.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:auth/di/auth_dependency_injection.dart';
+import 'package:transactions/di/transactions_dependency_injection.dart';
 import '../../firebase_options.dart';
 
 final it = GetIt.instance;
@@ -9,8 +11,15 @@ final it = GetIt.instance;
 Future<void> setupDI() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  await FirebaseAppCheck.instance.activate(
+    providerAndroid: AndroidDebugProvider(),
+    providerApple: AppleDebugProvider(),
+    providerWeb: ReCaptchaV3Provider('6LehZx0sAAAAALrk1LdvlzvlZB62bAygGUInUuYK'),
+  );
+
   it.registerLazySingleton(() => FirebaseAuth.instance);
 
-  AuthDependencyInjection.inject(it);
 
+  AuthDependencyInjection.inject(it);
+  TransactionsDependencyInjection.inject(it);
 }
