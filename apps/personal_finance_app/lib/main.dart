ce2baf7e848f8341ae5dc1920f49f22fff8222e9
Adrian Ruiz <addsdeveloper@gmail.com>
2025-12-01 09:20:23 -0500

    feat(app): initialize app with DI, providers and router

--- apps/personal_finance_app/lib/main.dart
+++ apps/personal_finance_app/lib/main.dart
@@ -1,20 +1,60 @@
+import 'package:auth/presentation/bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:personal_finance_app/config/router/app_router.dart';
+import 'package:personal_finance_app/di/dependency_injector.dart';
+import 'package:transactions/presentation/bloc/transaction_bloc.dart';
+import 'package:ui_kit/ui_kit.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupDI();
+  runApp(const FinanceApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FinanceApp extends StatelessWidget {
+  const FinanceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => it<AuthBloc>()..add(AuthSubscriptionRequested()),
         ),
-      ),
+        BlocProvider(
+          create: (context) => it<TransactionBloc>(),
+        ),
+      ],
+      child: _AppView(),
+    );
+  }
+}
+class _AppView extends StatefulWidget {
+  const _AppView();
+
+  @override
+  State<_AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<_AppView> {
+  late final AppRouter _appRouter;
+
+  @override
+  void initState() {
+    super.initState();
+    _appRouter = AppRouter(context.read<AuthBloc>());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Finanzas Fácil',
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
+      routerConfig: _appRouter.router,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
